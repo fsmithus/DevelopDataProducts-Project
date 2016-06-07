d193d6c180f8b805bf387018930b4b2f324f4e62
Fred Smith <us.frederick.smith@ieee.org>
2016-06-07 17:39:40 -0500

    Updated index.

--- ui.R
+++ ui.R
@@ -10,7 +10,10 @@ shinyUI(pageWithSidebar(
                 p("The top map illustrates the relative population with darker states having higher populations."),
                 p("The bottom map shows states that gained population in the preceding year in darker green, and shows lost population in a shade of red."),
                 p("The sequence can be animated by pressing the small right arrow on the right side of the slider above."),
-                a(href="http://www.census.gov/popest/data/national/totals/2015/","Based on data from the United States Census Bureau")
+                p(),
+                a(href="http://www.census.gov/popest/data/national/totals/2015/","Based on data from the United States Census Bureau"),
+                p(),
+                a(href="http://fsmithus.github.io/DevelopDataProducts-Project/index.html","Project index page")
         ),
         mainPanel(
                 plotOutput('map', width="100%")
